9dc0ef478122de095c250971a0363c6dfb778ae4
Mayo <61645609+codewithmayo@users.noreply.github.com>
2023-10-09 11:41:30 +0500

    Add files via upload

---
+++ nokia_snake_game.py
@@ -0,0 +1,50 @@
+import turtle
+from snake import Snake
+import time
+from food import Food
+from scoreboard import Scoreboard
+screen = turtle
+
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+screen.listen()
+
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
+
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        is_game_on = False
+
+    # detect collision with tail
+    for segment in snake.snake_segments[-1]:
+        if snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
+
+
---
+++ scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.goto(0, 260)
+        self.color("white")
+        self.update_scoreboard()
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.color("white")
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=("Courier", 24, "normal"))
+
+
