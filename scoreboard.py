d3048ce65ac9e4096cb67433d70cf8238efe4d75
Mayo <61645609+codewithmayo@users.noreply.github.com>
2023-10-11 09:24:14 +0500

    Update scoreboard.py ( high score saving system)

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,13 @@
 from turtle import Turtle
+highest_score = "data.txt"
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
+        self.get_high_score()
         self.penup()
         self.goto(0, 260)
         self.color("white")
@@ -12,16 +15,30 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.clear()
+        self.write(f"Current Score: {self.score} High Score: {self.high_score}", align="center", font=("Arial", 16, "normal"))
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.color("white")
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("Courier", 24, "normal"))
+    def check_high_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    def save_high_score(self):
+        with open(highest_score, "w") as file:
+            file.write(str(self.high_score))
+
+    def get_high_score(self):
+        with open(highest_score, "r") as file:
+            high_score_ever = file.read()
+            self.high_score = int(high_score_ever)
+
+
+
 
 
